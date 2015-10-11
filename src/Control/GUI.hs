156136960b9aaacb46ca7400cef97120e42d1286
Schell Scivally <efsubenovex@gmail.com>
2015-10-11 16:35:25 -0700

    api change - no merging functions

--- src/Control/GUI.hs
+++ src/Control/GUI.hs
@@ -18,6 +18,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TupleSections #-}
 module Control.GUI (
     -- * Creation
     -- $creation
@@ -28,8 +29,9 @@ module Control.GUI (
     -- * Combination
     -- $combination
     combineGUI,
-    combineGUIUntil,
     eitherGUI,
+    -- * Putting it all together
+    runGUIs,
     -- * Reexports
     module S,
 ) where
@@ -37,12 +39,13 @@ module Control.GUI (
 import Control.Varying as S
 import Control.Varying.Spline as S
 import Control.Arrow (first)
+import Control.Monad
 import Data.Renderable
 import Data.Monoid
 
 --------------------------------------------------------------------------------
 -- $creation
--- In order to create a SplineT you must first have a datatype with a 'Composite'
+-- In order to create a spline you must first have a datatype with a 'Composite'
 -- instance. Then you must create a 'varying' value of that datatype
 -- describing how it will change over the domain of user input and time.
 -- Also needed is an event stream that eventually ends the user's interaction.
@@ -50,28 +53,25 @@ import Data.Monoid
 -- eventually produces a result value that can be used in a monadic
 -- sequence.
 --------------------------------------------------------------------------------
--- | Creates a new SplineT displaying an interface that eventually produces a value.
--- The type used to represent the user interface must have a 'Decomposable'
--- instance, that way the resulting GUI\'s discrete values can be rendered.
+-- | Create a spline describing an interface that eventually produces a value.
+-- The type used to represent the user interface must have a 'Composite'
+-- instance. This allows GUIs to be layered graphically since separate
+-- GUI's iteration values can all be combined after being broken down into
+-- transformed primitives.
 gui :: (Monad m, Monad n, Monoid (f (t, Element n r t)), Composite a f n r t)
     => Var m i a
     -- ^ The stream of a changing user interface.
     -> Var m i (Event b)
     -- ^ The event stream that concludes a user\'s interaction. When this
-    -- stream produces an event the interaction will end and the merging
-    -- function will be used to create the GUI\'s return type.
-    -> (a -> b -> c)
-    -- ^ The merging function that combines the interface's final value with the
-    -- value produced by the event stream.
-    -> SplineT m f i (t, Element n r t) c
-gui v ve f = SplineT $ Var $ \i -> do
-    (a, v')  <- runVar v i
-    (e, ve') <- runVar ve i
-    let ui = composite a
-    return $ case e of
-        NoEvent -> (Step ui NoEvent, runSplineT $ gui v' ve' f)
-        Event b -> let ex = Event $ f a b
-                   in (Step ui ex, pure $ Step mempty ex)
+    -- stream produces an event the interaction will end and the spline
+    -- will conclude.
+    -> SplineT m f i (t, Element n r t) (a,b)
+gui v ve = SplineT $ t ~> var (uncurry f)
+    where t = (,) <$> v <*> ve
+          f a e = let ui = composite a
+                  in case e of
+                         NoEvent -> Step ui NoEvent
+                         Event b -> Step ui $ Event (a, b)
 
 --------------------------------------------------------------------------------
 -- $transformation
@@ -90,56 +90,28 @@ transformGUI vt g = SplineT $ Var $ \i -> do
     return (Step ui' e, runSplineT $ transformGUI vt' $ SplineT v)
 --------------------------------------------------------------------------------
 -- $combination
--- Combining two GUIs creates a new GUI. The strategy taken to combine the
--- result values of the two GUIs must be provided. With 'combineGUI' the
--- SplineT runs until both component GUIs have concluded. With 'combineGUIUntil'
--- the SplineT runs until either both component GUIs have concluded or
--- until a switching event occurs.
 --------------------------------------------------------------------------------
--- | Combines two GUIs. The resulting SplineT will not produce a value until
--- both component GUIs have produced a value. At that moment a merging function
--- is used to combine the two values into the resulting GUI\'s return type.
--- The component GUIs\' graphical representations (the left Step values) are
--- 'mappend'\d together.
-combineGUI :: Monad m
-           => SplineT m f i u a
-           -- ^ The first GUI.
-           -> SplineT m f i u b
-           -- ^ The second GUI.
-           -> (a -> b -> c)
-           -- ^ The merging function.
-           -> SplineT m f i u c
-combineGUI (SplineTConst a) (SplineTConst b) f = SplineTConst $ f a b
-combineGUI (SplineTConst a) (SplineT vb) f = SplineT $ fmap (fmap (f a)) vb
-combineGUI (SplineT va) (SplineTConst b) f = SplineT $ fmap (fmap (`f` b)) va
-combineGUI (SplineT va) (SplineT vb) f = SplineT $ Var $ \i -> do
-        (Step a ea, va') <- runVar va i
-        (Step b eb, vb') <- runVar vb i
-        return (Step (a <> b) (f <$> ea <*> eb),
-                runSplineT $ combineGUI (SplineT va') (SplineT vb') f)
-
 -- | Combines two GUIs. The resulting SplineT will not produce a value until
 -- either both component GUIs have produced a value, or until a switching
 -- event occurs. At that moment a merging function is used to combine any
 -- available produced values in the resulting GUI\'s return type.
 -- The component GUIs\' graphical representations (the left Step values) are
 -- 'mappend'\d together.
-combineGUIUntil :: (Monad m, Monoid (f u))
+combineGUI :: (Monad m, Monoid (f u))
                 => SplineT m f i u a
                 -> SplineT m f i u b
                 -> Var m i (Event c)
-                -> (Maybe a -> Maybe b -> Maybe c -> d)
-                -> SplineT m f i u d
-combineGUIUntil (SplineTConst a) (SplineTConst b) _ f =
-    SplineTConst $ f (Just a) (Just b) Nothing
-combineGUIUntil (SplineTConst a) s vc f =
-    combineGUIUntil (SplineT $ pure $ Step mempty $ Event a) s vc f
-combineGUIUntil s (SplineTConst b) vc f =
-    combineGUIUntil s (SplineT $ pure $ Step mempty $ Event b) vc f
-combineGUIUntil (SplineT va) (SplineT vb) vc f = SplineT $ Var $ \i -> do
+                -> SplineT m f i u (Maybe a, Maybe b, Maybe c)
+combineGUI (SplineTConst a) (SplineTConst b) _ =
+    SplineTConst (Just a, Just b, Nothing)
+combineGUI (SplineTConst a) s vc =
+    combineGUI (SplineT $ pure $ Step mempty $ Event a) s vc
+combineGUI s (SplineTConst b) vc =
+    combineGUI s (SplineT $ pure $ Step mempty $ Event b) vc
+combineGUI (SplineT va) (SplineT vb) vc = SplineT $ Var $ \i -> do
     (Step ua ea, va') <- runVar va i
     (Step ub eb, vb') <- runVar vb i
-    (ec, vc')       <- runVar vc i
+    (ec, vc')         <- runVar vc i
     let g ex v = case ex of
                      Event x -> pure $ Step mempty $ Event x
                      NoEvent -> v
@@ -148,15 +120,16 @@ combineGUIUntil (SplineT va) (SplineT vb) vc f = SplineT $ Var $ \i -> do
         ma = toMaybe ea
         mb = toMaybe eb
         mc = toMaybe ec
+        ev = Event (ma,mb,mc)
         e  = case (ma,mb,mc) of
-                 (Just _, Just _,      _) -> Event $ f ma mb mc
-                 (     _,      _, Just _) -> Event $ f ma mb mc
+                 (Just _, Just _,      _) -> ev
+                 (     _,      _, Just _) -> ev
                  _                        -> NoEvent
-    return (Step (ua <> ub) e, runSplineT $ combineGUIUntil (SplineT va'') (SplineT vb'') vc' f)
+    return (Step (ua <> ub) e, runSplineT $ combineGUI (SplineT va'') (SplineT vb'') vc')
 
 -- | Run two GUIs at the same time and return the result of the SplineT that
--- concludes first. If they're both concluded the result is taken from the
--- GUI on the left.
+-- concludes first. If they conclude at the same time the result is taken from
+-- the GUI on the left.
 eitherGUI :: (Monad m, Monoid (f u))
           => SplineT m f i u a -> SplineT m f i u a -> SplineT m f i u a
 eitherGUI (SplineTConst a) s =
@@ -170,4 +143,30 @@ eitherGUI (SplineT va) (SplineT vb) = SplineT $ Var $ \i -> do
         (Event _,_) -> return (Step (ua <> ub) ea, va')
         (_,Event _) -> return (Step (ua <> ub) eb, vb')
         (_,_)       -> return (Step (ua <> ub) NoEvent,
-                               runSplineT $ eitherGUI (SplineT va') (SplineT vb') )
+                               runSplineT $ eitherGUI (SplineT va') (SplineT vb'))
+
+-- | Run a list of GUIs in parallel. Restart individual GUIs whenever it
+-- concludes in a value. Return a list of the most recent result values once the
+-- control GUI concludes.
+runGUIs :: (Monad m, Monoid (f b))
+        => [Maybe c -> SplineT m f a b c] -> SplineT m f a b ()
+        -> SplineT m f a b [Maybe c]
+runGUIs gs = go gs es $ zipWith ($) gs xs
+    where es = replicate n NoEvent
+          xs = replicate n Nothing
+          n  = length gs
+          go fs evs guis egui = SplineT $ Var $ \a -> do
+            let step (ecs, fb, vs) (f, ec, g) = do
+                    (Step fb' ec', v) <- runVar (runSplineT g) a
+                    let ec'' = ec <> ec'
+                        fb'' = fb <> fb'
+                        v'   = case ec' of
+                                   NoEvent -> v
+                                   Event c -> runSplineT $ f $ Just c
+                    return (ecs ++ [ec''], fb'', vs ++ [SplineT v'])
+            (ecs, fb, guis') <- foldM step ([],mempty,[]) (zip3 fs evs guis)
+            (Step fb' ec, v) <- runVar (runSplineT egui) a
+            let fb'' = fb <> fb'
+                ec' = map toMaybe ecs <$ ec
+            return (Step fb'' ec',
+                    runSplineT $ go fs ecs guis' $ SplineT v)
